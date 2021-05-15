b30097b40c61aebf74d86d96298138a3ed4cd740
mehran13mome <mehran13mome@gmail.com>
2021-05-16 01:29:41 +0430

    Fix main product to the middle

--- src/components/loginLayout.js
+++ src/components/loginLayout.js
@@ -9,9 +9,9 @@ const LoginLayout = () => {
                         <img className='mt-5 ' src="/test.svg" alt=""/>
                     </div>
                     <div className='col-md-6 text-center p-0 m-0'>
-                        <img width='80px' height='80px' src="/test.svg" alt=""/>
+                        <img width='80px' height='80px' src="/loginface.svg" alt=""/>
                         <p>Sign in to Your account</p>
-                        <form className='input-group w-100' action="">
+                        <form className='input-group w-100 mt-4' action="">
                             <input className='w-75 input-border rounded-pill mt-1 mx-auto' type="email" placeholder='Email'/>
                             <input className='w-75 input-border rounded-pill mt-3 mx-auto' type="password" placeholder='Password'/>
                             <a className='mt-4 mx-auto w-100 mb-4' href="#">Forgot Password?</a>
@@ -21,20 +21,10 @@ const LoginLayout = () => {
                                     <input className='offset-1 button-signup w-25 rounded-pill' type="button" value='Sign up'/>
                                 </Link>
                             </div>
-                            <p style={{color:'#9e9e9e'}} className='w-50 mx-auto mt-4 user-select-none'>OR LOGIN WITH</p>
-
-                            <div className='w-100'>
-                                <button className='rounded-circle shadow other-logins fw-bold'>F</button>
-                                <button className='rounded-circle shadow other-logins'>G</button>
-                                <button className='rounded-circle shadow other-logins '>T</button>
-                            </div>
-
-
                         </form>
                     </div>
             </div>
         </div>
-
     );
 };
 
