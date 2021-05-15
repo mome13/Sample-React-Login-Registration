da9cf437d6a5d9b9c3126988fae33ee9a0dd4bcb
mehran13mome <mehran13mome@gmail.com>
2021-05-16 01:55:01 +0430

    Add demo png of project

--- src/components/RegisterLayout.js
+++ src/components/RegisterLayout.js
@@ -9,7 +9,7 @@ const RegisterLayout = () => {
                     <img className='mt-5 ' src="/test.svg" alt=""/>
                 </div>
                 <div className='col-md-6 text-center p-0 m-0'>
-                    <img width='80px' height='80px' src="/test.svg" alt=""/>
+                    <img width='80px' height='80px' src="/loginface.svg" alt=""/>
                     <p>Sign in to Your account</p>
                     <form className='input-group w-100' action="">
                         <input className='w-75 input-border rounded-pill mt-1 mx-auto' type="email" placeholder='Email'/>
@@ -19,13 +19,6 @@ const RegisterLayout = () => {
                             <Link to="/signup">
                                 <input className='button-signin w-25 rounded-pill' type="submit" value='Sign up'/>
                             </Link>
-                        </div>
-                        <p style={{color:'#9e9e9e'}} className='w-50 mx-auto mt-4 user-select-none'>OR LOGIN WITH</p>
-
-                        <div className='w-100'>
-                            <button className='rounded-circle shadow other-logins fw-bold'>F</button>
-                            <button className='rounded-circle shadow other-logins'>G</button>
-                            <button className='rounded-circle shadow other-logins '>T</button>
                         </div>
 
                     </form>
